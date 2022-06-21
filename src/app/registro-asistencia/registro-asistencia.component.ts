fd26a017b9efd6def4de60798e3fa4622b43fdbf
fsdominguez12 <franklin.dominguez.est@tecazuay.edu.ec>
2022-06-21 16:51:16 -0500

    Anexo 9

--- src/app/registro-asistencia/registro-asistencia.component.ts
+++ src/app/registro-asistencia/registro-asistencia.component.ts
@@ -6,7 +6,14 @@ import { RegistroAsistenciaService } from '../services/registro-asistencia.servi
 import swal from 'sweetalert2';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActividadesDiarias } from '../models/actividades-diarias';
+import Docxtemplater from "docxtemplater";
+import PizZip from "pizzip";
+import PizZipUtils from "pizzip/utils/index.js";
+import { saveAs } from "file-saver";
 
+function loadFile(url, callback) {
+  PizZipUtils.getBinaryContent(url, callback);
+}
 
 @Component({
   selector: 'app-registro-asistencia',
@@ -162,6 +169,68 @@ export class RegistroAsistenciaComponent implements OnInit {
         
       }
     })
+  }
+
+
+  //metodo generar documento
+
+  generate(nom:any) {
+    
+    loadFile("https://backendg1c2.herokuapp.com/files/anexo3.docx", function(
+      error,
+      content
+    ) {
+      if (error) {
+        throw error;
+      }
+
+
+  
+      const zip = new PizZip(content);
+      const doc = new Docxtemplater(zip, { paragraphLoop: true, linebreaks: true });
+      doc.setData({
+        rpp: nom,
+        numestudiantes: "2501"
+      });
+      try {
+        // Se reemplaza en el documento: {rpp} -> John, {numestudiantes} -> Doe ....
+        doc.render();
+      } catch (error) {
+        // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
+        function replaceErrors(key, value) {
+          if (value instanceof Error) {
+            return Object.getOwnPropertyNames(value).reduce(function(
+              error,
+              key
+            ) {
+              error[key] = value[key];
+              return error;
+            },
+            {});
+          }
+          return value;
+        }
+        console.log(JSON.stringify({ error: error }, replaceErrors));
+
+        if (error.properties && error.properties.errors instanceof Array) {
+          const errorMessages = error.properties.errors
+            .map(function(error) {
+              return error.properties.explanation;
+            })
+            .join("\n");
+          console.log("errorMessages", errorMessages);
+
+        }
+        throw error;
+      }
+      const out = doc.getZip().generate({
+        type: "blob",
+        mimeType:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      });
+      // Output the document using Data-URI
+      saveAs(out, "anexo9.docx");
+    });
   }
 
 
