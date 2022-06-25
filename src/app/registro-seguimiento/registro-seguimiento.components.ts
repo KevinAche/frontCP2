6869824c7996fa432082c894475298248907e872
vero-sari <81376240+vero-sari@users.noreply.github.com>
2022-06-24 19:05:58 -0500

    Anexo10

--- src/app/registro-seguimiento/registro-seguimiento.components.ts
+++ src/app/registro-seguimiento/registro-seguimiento.components.ts
@@ -8,7 +8,17 @@ import { left } from '@popperjs/core';
 import { TutorA } from '../models/TutorA';
 import { TutorAService } from '../services/tutorA.service';
 import { ActividadesCronograma } from '../models/ActividadesCronograma';
+import Docxtemplater from "docxtemplater";
+import PizZip from "pizzip";
+import PizZipUtils from "pizzip/utils/index.js";
+import { saveAs } from "file-saver";
+import { empty, Observable, ReplaySubject } from 'rxjs';
+import { Empresa } from '../services/empresa';
 
+
+function loadFile(url, callback) {
+  PizZipUtils.getBinaryContent(url, callback);
+}
 @Component({
   selector: 'app-info-reg-seguimiento',
   templateUrl: './registro-seguimiento.components.html',
@@ -25,6 +35,8 @@ export class RegistroSeguimientoAlumnoComponent implements OnInit {
   tutoracademico: any;
   tutora: TutorA = new TutorA();
   cronograma: ActividadesCronograma = new ActividadesCronograma();
+  base64Output : string;
+  formCronograma: FormGroup;
 
 
   public cedulaAlumno: any;
@@ -102,6 +114,164 @@ export class RegistroSeguimientoAlumnoComponent implements OnInit {
     console.log(this.cronograma);
 
   }
+ /* generate(nom:any,em:any,nt:any) {
+    var est=this.estudiante;
+    var tut=this.tutora;
+    var te=this.tutore
+    if (this.formCronograma.invalid) {
+      lc.fire(
+        'Error de entrada',
+        'Revise que los campos no esten vacios',
+        'error'
+      )
+      return;
+    }
+    loadFile("https://backendg1c2.herokuapp.com/files/anexo10.docx", function(
+      error,
+      content
+    ) {
+      if (error) {
+        throw error;
+      }
+      const zip = new PizZip(content);
+      const doc = new Docxtemplater(zip, { paragraphLoop: true, linebreaks: true });
+      doc.setData({
+        nombreAlumno: nom,
+        nombreEmpresa:em,
+        nombreTutor:nt
+      });
+      try {
+        // Se reemplaza en el documento: {rpp} -> John, {numestudiantes} -> Doe ....
+        doc.render();
+      } catch (error) {
+        // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
+        function replaceErrors(key, value) {
+          if (value instanceof Error) {
+            return Object.getOwnPropertyNames(value).reduce(function(
+              error,
+              key
+            ) {
+              error[key] = value[key];
+              return error;
+            },
+            {});
+          }
+          return value;
+        }
+        console.log(JSON.stringify({ error: error }, replaceErrors));
 
+        if (error.properties && error.properties.errors instanceof Array) {
+          const errorMessages = error.properties.errors
+            .map(function(error) {
+              return error.properties.explanation;
+            })
+            .join("\n");
+          console.log("errorMessages", errorMessages);
+
+        }
+        throw error;
+      }
+      const out = doc.getZip().generate({
+        type: "blob",
+        mimeType:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      });
+      // Output the document using Data-URI
+      saveAs(out, "anexo3.docx");
+    });
+  }
+
+
+
+
+  //Convertir a base 64 un documento
+
+  onFileSelected(event) {
+    this.convertFile(event.target.files[0]).subscribe(base64 => {
+      this.base64Output = base64;
+
+    });
+  }
+
+  convertFile(file : File) : Observable<string> {
+    const result = new ReplaySubject<string>(1);
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = (event) => result.next(btoa(event.target.result.toString()));
+    return result;
+  }*/
+  generate(nom,em,nt) {
+    loadFile("https://backendg1c2.herokuapp.com/files/anexo10.docx", function(
+      error,
+      content
+    ) {
+      if (error) {
+        throw error;
+      }
+
+
+  
+      const zip = new PizZip(content);
+      const doc = new Docxtemplater(zip, { paragraphLoop: true, linebreaks: true });
+      doc.setData({
+        nombreAlumno: nom,
+        nombreEmpresa: em,
+        nombreTutor:nt
+      });
+      try {
+        // Se reemplaza en el documento: {rpp} -> John, {numestudiantes} -> Doe ....
+        doc.render();
+      } catch (error) {
+        // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
+        function replaceErrors(key, value) {
+          if (value instanceof Error) {
+            return Object.getOwnPropertyNames(value).reduce(function(
+              error,
+              key
+            ) {
+              error[key] = value[key];
+              return error;
+            },
+            {});
+          }
+          return value;
+        }
+        console.log(JSON.stringify({ error: error }, replaceErrors));
+
+        if (error.properties && error.properties.errors instanceof Array) {
+          const errorMessages = error.properties.errors
+            .map(function(error) {
+              return error.properties.explanation;
+            })
+            .join("\n");
+          console.log("errorMessages", errorMessages);
+
+        }
+        throw error;
+      }
+      const out = doc.getZip().generate({
+        type: "blob",
+        mimeType:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      });
+      // Output the document using Data-URI
+      saveAs(out, "anexo10.docx");
+    });
+  }
+
+  //Subir un archivo a la base
+
+  onFileSelected(event) {
+    this.convertFile(event.target.files[0]).subscribe(base64 => {
+      this.base64Output = base64;
+    });
+  }
+
+  convertFile(file : File) : Observable<string> {
+    const result = new ReplaySubject<string>(1);
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = (event) => result.next(btoa(event.target.result.toString()));
+    return result;
+  }
 }
-
